0f15b19596e7975456cfb8af0af64d96c7f8082c
SpagetCoder <quassa.mody@gmail,com>
2019-04-25 23:53:02 +0200

    added few new algorithms

--- QuickSortingAlgorithms/Źródło.cpp
+++ QuickSortingAlgorithms/Źródło.cpp
@@ -5,56 +5,98 @@
 #include <vector>
 #include <string>
 #include <fstream>
+#include <algorithm>
+
 
 using namespace std;
 
-int partition(vector<int>&data, int low, int high)
+int medianPivot(vector<int>&data, int left, int right)
 {
-	int pivot = data[high]; // pivot 
-	int i = (low - 1); // Index of smaller element 
+	srand(time(NULL));
+	int a = left + rand() % (right - left);
+	int b = min(left + 1 + rand() % (right - left),right);
+	int c = min(left + 2 + rand() % (right - left), right);
+	int median;
 
-	for (int j = low; j <= high - 1; j++) {
+	if (a < b && a > c || a > b && a < c)
+	{
+		median = a;
+	}
+	else if (b < a && b > c || b > a && b < c)
+	{
+		median = b;
+	}
+	else
+	{
+		median = c;
+	}
 
-		// If current element is smaller than or 
-		// equal to pivot 
-		if (data[j] <= pivot) {
+	swap(data[median], data[right]);
 
-			i++; // increment index of smaller element 
+	int pivot = data[right];
+	int i = (left - 1); 
+
+	for (int j = left; j <= right - 1; j++)
+	{
+
+		if (data[j] <= pivot)
+		{
+			i++;
 			swap(data[i], data[j]);
 		}
 	}
-	swap(data[i + 1], data[high]);
+	swap(data[i + 1], data[right]);
 	return (i + 1);
 }
 
-int partition_r(vector<int>&data, int low, int high)
+void quicksortMedian(vector<int>& data, int left, int right)
 {
-	// Generate a random number in between 
-	// low .. high 
+	int pivot;
+
+	if (left < right)
+	{
+		pivot = medianPivot(data, left, right);
+
+		quicksortMedian(data, left, pivot - 1);
+		quicksortMedian(data, pivot + 1, right);
+	}
+}
+
+int randomPivot(vector<int>&data, int left, int right)
+{
 	srand(time(NULL));
-	int random = low + rand() % (high - low);
+	int random = left + rand() % (right - left);
+	swap(data[random], data[right]);
 
-	// Swap A[random] with A[high] 
-	swap(data[random], data[high]);
+	int pivot = data[right]; 
+	int i = (left - 1); 
 
-	return partition(data, low, high);
+	for (int j = left; j <= right - 1; j++) 
+	{
+
+		if (data[j] <= pivot)
+		{
+			i++; 
+			swap(data[i], data[j]);
+		}
+	}
+	swap(data[i + 1], data[right]);
+	return (i + 1);
 }
 
 void quicksortWithRandomPivot(vector<int>& data, int left, int right)
 {
-	int pivot, s;
-
-	srand(time(NULL));
+	int pivot;
 
 	if (left < right)
 	{
-		pivot = partition_r(data, left, right);
+		pivot = randomPivot(data, left, right);
 
-
-		quicksortWithRandomPivot(data, left, pivot-1);
+		quicksortWithRandomPivot(data, left, pivot - 1);
 		quicksortWithRandomPivot(data, pivot + 1, right);
 	}
 }
+
 
 void quicksort(vector<int>& data, int left, int right)
 {
@@ -79,6 +121,53 @@ void quicksort(vector<int>& data, int left, int right)
 	}
 }
 
+void insertionSort(vector <int>& data)
+{
+	int value_min, j;
+
+	for (int i = 1; i < data.size(); i++)
+	{
+		value_min = data[i];
+		j = i - 1;
+
+		while (j >= 0 && value_min < data[j])
+		{
+			data[j + 1] = data[j];
+			j--;
+		}
+		data[j + 1] = value_min;
+	}
+
+}
+
+void quicksortWithInsertion(vector<int>& data, int left, int right)
+{
+	int pivot, s;
+
+	if (left < right)
+	{
+
+		if (right - left < 10)
+			insertionSort(data);
+
+		pivot = data[left];
+		s = left;
+
+		for (int i = left + 1; i < right; i++)
+		{
+			if (data[i] < pivot)
+			{
+				s++;
+				swap(data[s], data[i]);
+			}
+		}
+		swap(data[s], data[left]);
+		quicksort(data, left, s);
+		quicksort(data, s + 1, right);
+	}
+}
+
+
 void check_if_sorted(vector<int>& data)
 {
 	int i = 1;
@@ -100,7 +189,6 @@ void check_if_sorted(vector<int>& data)
 
 }
 
-
 int main(int argc, char *argv[])
 {
 	int size;
@@ -118,26 +206,37 @@ int main(int argc, char *argv[])
 	}
 
 	auto start = chrono::high_resolution_clock::now();
-
 	data2 = data;
 	quicksort(data2, 0, data2.size());
 	auto finish = chrono::high_resolution_clock::now();
 	chrono::duration<double> elapsed = finish - start;
 	cout << "quicksort sort time " << elapsed.count() << endl;
-
 	check_if_sorted(data2);
 
 
 	start = chrono::high_resolution_clock::now();
-
 	data2 = data;
 	quicksortWithRandomPivot(data2, 0, data2.size());
 	finish = chrono::high_resolution_clock::now();
 	elapsed = finish - start;
 	cout << "quicksort sort with random pivot time " << elapsed.count() << endl;
-
 	check_if_sorted(data2);
 
+	start = chrono::high_resolution_clock::now();
+	data2 = data;
+	quicksortMedian(data2, 0, data2.size());
+	finish = chrono::high_resolution_clock::now();
+	elapsed = finish - start;
+	cout << "quicksort sort with median of three " << elapsed.count() << endl;
+	check_if_sorted(data2);
+
+	start = chrono::high_resolution_clock::now();
+	data2 = data;
+	quicksortWithInsertion(data2, 0, data2.size());
+	finish = chrono::high_resolution_clock::now();
+	elapsed = finish - start;
+	cout << "quicksort sort with insertion " << elapsed.count() << endl;
+	check_if_sorted(data2);
 
 	system("pause");
 
